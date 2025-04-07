bfc4bb73f16af827ffb5c982d871d2036c2bef11
egor <ternovskiy.egor555@gmail.com>
2025-04-07 22:10:32 +0300

    Secure Voting Node Initialization

---
+++ src/main/java/edu/karazin/secure_voting_node/SecureVotingNodeApplication.java
@@ -0,0 +1,13 @@
+package edu.karazin.secure_voting_node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureVotingNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureVotingNodeApplication.class, args);
+	}
+
+}
